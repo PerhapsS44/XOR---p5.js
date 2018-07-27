8966278ce2e46ad359ea1058fcbd30cf2ed86d40
PerhapsS44 <41445177+PerhapsS44@users.noreply.github.com>
2018-07-27 15:49:21 +0300

    Changes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,33 @@
+var nn;
+var inputs = [];
+var targets = [];
+
 function setup() {
-  // put setup code here
+  nn = new NeuralNetwork(2,2,1);
+  for (var i=0;i<1000;i++){
+    chooseRandomTrainingSet();
+    nn.evolve(inputs,taragets)
+  }
+}
+
+function chooseRandomTrainingSet(){
+  var r = random(0,10);
+  if (r < 2.5){
+    inputs = [1,1];
+    targets = [0];
+  }
+  else if (r < 5){
+    inputs = [0,1];
+    targets = [1];
+  }
+  else if (r < 7.5){
+    inputs = [1,0];
+    targets = [1];
+  }
+else{
+    inputs = [0,0];
+    targets = [0];
+  }
 }
 
 function draw() {
